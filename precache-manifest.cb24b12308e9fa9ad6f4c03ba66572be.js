c302ad46b2162de185649e93c5959f82ff2998f4
Chris <stephens-c2@live.com>
2020-04-22 22:03:28 -0700

    initial commit

---
+++ precache-manifest.cb24b12308e9fa9ad6f4c03ba66572be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca18c255e3670515b49d246193f174a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59faaa45c544f45bb33e",
+    "url": "/static/css/main.3f3b5814.chunk.css"
+  },
+  {
+    "revision": "ae747b7a395d2810ab64",
+    "url": "/static/js/2.bcf9cefa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bcf9cefa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59faaa45c544f45bb33e",
+    "url": "/static/js/main.cc5e05b0.chunk.js"
+  },
+  {
+    "revision": "889a957863a5c1dfb747",
+    "url": "/static/js/runtime-main.125715d9.js"
+  },
+  {
+    "revision": "b8b85cf3dde8d3c400d6a490fdc8d6e3",
+    "url": "/static/media/portfolio.b8b85cf3.jpg"
+  }
+]);

c0d5bbac4dfe53a6f8a2d15fe188db9d84e0e5dc
Chris <stephens-c2@live.com>
2020-04-22 22:18:03 -0700

    Updates

---
+++ precache-manifest.470c5a1551ea1610799564ece7755d25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4165c960327c8fb1ec2a492a44331163",
+    "url": "/React-portfolio/index.html"
+  },
+  {
+    "revision": "237277b3d0fe0b51e925",
+    "url": "/React-portfolio/static/css/main.3f3b5814.chunk.css"
+  },
+  {
+    "revision": "ae747b7a395d2810ab64",
+    "url": "/React-portfolio/static/js/2.bcf9cefa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-portfolio/static/js/2.bcf9cefa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "237277b3d0fe0b51e925",
+    "url": "/React-portfolio/static/js/main.ea0f8180.chunk.js"
+  },
+  {
+    "revision": "d60327c0faf3cffd4199",
+    "url": "/React-portfolio/static/js/runtime-main.bec08502.js"
+  },
+  {
+    "revision": "b8b85cf3dde8d3c400d6a490fdc8d6e3",
+    "url": "/React-portfolio/static/media/portfolio.b8b85cf3.jpg"
+  }
+]);
